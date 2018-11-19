4ba65f32ee85f02bbdbb5e1aec01a2873950b113
Melissa George <melissalgeorge8@gmail.com>
2018-11-19 13:00:00 -0700

    changed settings

--- mrp/settings/base.py
+++ mrp/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['time-mrp.herokuapp.com']
+ALLOWED_HOSTS = ['warm-shore-79208.herokuapp.com']
 
 
 # Application definition
