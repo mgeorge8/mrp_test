ed16c55d5c95c01c05ff193877cf45abd35c6c09
Ryan George <ryan.kris.george@gmail.com>
2018-12-26 15:45:15 -0700

    add export for models

--- mrp_system/admin.py
+++ mrp_system/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from mrp_system.models import (Part, Location,
-Manufacturer, ManufacturerRelationship, Type)
+from mrp_system.models import (Part, Location, LocationRelationship,
+Manufacturer, ManufacturerRelationship, Type, Field)
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
@@ -37,9 +37,32 @@ class LocationResource(resources.ModelResource):
 class LocationAdmin(ImportExportModelAdmin):
     resource_class = LocationResource
 
+class LocationRelationshipResource(resources.ModelResource):
+    class Meta:
+        model = LocationRelationship    
+
+class LocationRelationshipAdmin(ImportExportModelAdmin):
+    resource_class = LocationRelationshipResource
+
+class TypeResource(resources.ModelResource):
+    class Meta:
+        model = Type    
+
+class TypeAdmin(ImportExportModelAdmin):
+    resource_class = TypeResource
+
+class FieldResource(resources.ModelResource):
+    class Meta:
+        model = Field    
+
+class FieldAdmin(ImportExportModelAdmin):
+    resource_class = FieldResource
+
     
 admin.site.register(Part, PartAdmin)
 admin.site.register(Manufacturer, ManufacturerAdmin)
 admin.site.register(ManufacturerRelationship, ManufacturerRelationshipAdmin)
 admin.site.register(Location, LocationAdmin)
-admin.site.register(Type)
+admin.site.register(LocationRelationship, LocationRelationshipAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Field, FieldAdmin)
